e841dea53122ae2b1a308339850db05c7e95c9fd
av02 <39122034+av02@users.noreply.github.com>
2021-09-26 04:28:06 +0200

    Update mainv2.py

--- mainv2.py
+++ mainv2.py
@@ -3,11 +3,17 @@ import database_outils#?
 import boucle_infinie_coc
 import bot_discord
 # import donnés
-from config import config
+#from config import config
 
 
 
 
+config={"Coc":{"mail":os.environ.get("mail"),
+              "password":os.environ.get("password")},
+        "Discord":{"token":os.environ.get("Token"),
+                  "prefix":os.environ.get("prefix")},
+        "bddlink":os.environ.get("DATABASE_URL")
+       }
 
 def main():
     
