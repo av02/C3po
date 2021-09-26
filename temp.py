94c030ec6bb6fb366bfd59af2ed1a44c277b5381
av02 <39122034+av02@users.noreply.github.com>
2021-09-26 03:20:56 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ import psycopg2
 
 connectionBDD = psycopg2.connect(os.environ.get("DATABASE_URL"),sslmode='require')
 Curseur = connectionBDD.cursor()
-Curseur.execute("""CREATE TABLE "new" ( 'tagIG' TEXT, 'discordID' INTEGER, 'pseudoIG' TEXT, 'thIG' INTEGER, 'donne' INTEGER DEFAULT 0, 'recu' INTEGER DEFAULT 0,
+Curseur.execute("""CREATE TABLE 'new' ( 'tagIG' TEXT, 'discordID' INTEGER, 'pseudoIG' TEXT, 'thIG' INTEGER, 'donne' INTEGER DEFAULT 0, 'recu' INTEGER DEFAULT 0,
                 'nbattaqueshdv' INTEGER DEFAULT 0, 'perfhdv' INTEGER DEFAULT 0, 'bihdv' INTEGER DEFAULT 0, 'onehdv' INTEGER DEFAULT 0, 'blackhdv' INTEGER DEFAULT 0,
                 'nbattaqueshdv-1' INTEGER DEFAULT 0, 'perfhdv-1' INTEGER DEFAULT 0, 'bihdv-1' INTEGER DEFAULT 0, 'onehdv-1' INTEGER DEFAULT 0, 'blackhdv-1' INTEGER DEFAULT 0, 
                 'perfdefhdv' INTEGER DEFAULT 0, 'nbdefhdv' INTEGER DEFAULT 0, 'clan' TEXT, 'attaqueshdvante' INTEGER, 'perfhdvante' INTEGER, 'bihdvante' INTEGER, 
