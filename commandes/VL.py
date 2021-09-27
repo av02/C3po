c20c181947ac31b502a3d8226b4182137efad602
av02 <39122034+av02@users.noreply.github.com>
2021-09-27 20:59:14 +0200

    Update VL.py

--- commandes/VL.py
+++ commandes/VL.py
@@ -16,15 +16,15 @@ def display_str_calibrated(chaine:str,longeur:int)->str:
     return " "*longeur
 
 async def VL(DiscordClient,message,args):
-    if args==[] or int(args[0])>15 or int(args[0]) < 2 :
+    if args==[] or int(args[1])>15 or int(args[1]) < 2 :
         return
     dips = False
-    if len(args)>1 and args[1]=="dips":
+    if len(args)>2 and args[2]=="dips":
         dips=True
-    liste=DiscordClient.connectionBDD.get_classement_attaques(int(args[0]), dips, limit=10, clan=None, nb_etoiles=3)
+    liste=DiscordClient.connectionBDD.get_classement_attaques(int(args[1]), dips, limit=10, clan=None, nb_etoiles=3)
     if len(liste)==0:
         return await message.channel.send("pas de donnés")
-    reponse = "      __**classement des membres hdv {}{}**__".format(int(args[0])," dips" if dips else "")
+    reponse = "      __**classement des membres hdv {}{}**__".format(int(args[1])," dips" if dips else "")
     reponse +="```{}| 3 | 2 | 1 | 0 |nb |tag".format(display_str_calibrated("pseudo",33))
     for e in liste:
         nom=e[2]
