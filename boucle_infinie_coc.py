feb72d99de1398875cc00071bd86f608dcf91eb2
av02 <39122034+av02@users.noreply.github.com>
2021-10-08 22:29:44 +0200

    Update boucle_infinie_coc.py

--- boucle_infinie_coc.py
+++ boucle_infinie_coc.py
@@ -66,7 +66,7 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
     
     
     @cocClient.event  
-    @coc.PlayerEvents.clan(tags=tagsJoueur)
+    @coc.PlayerEvents.clan(tags=tagsJoueurs)
     async def on_clan_status_change(old,new):
         print("il y a {} qui a quitté {} ou rejoint {}".format(old.name,old.clan,new.clan))
         pass
