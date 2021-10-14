fe05a6e01264b50a5be8da830c530d250413addc
av02 <39122034+av02@users.noreply.github.com>
2021-10-15 00:59:18 +0200

    Update gc.py

--- commandes/gc.py
+++ commandes/gc.py
@@ -23,7 +23,7 @@ async def gc(DiscordClient, message,args):#TODO: ajouter le cas de l'autotag, pa
     rep.set_author(name="Profil de "+str(pseudo))
     tags = map(lambda tupple_data:tupple_data[0],liste_comptes)
     async for player in DiscordClient.cocClient.get_players(tags):
-        rep.add_field(name=player.name,value="<:HdvBot:884202091793506324> Hdv : {} \n<:ExpBot:884202964896608266> Niveau : {} \n<:TagBot:884204003070705754> Tag : {}\n🛡️ Clan:{}\n<:3Stars:898336084897984574> %perf:NA %".format(player.town_hall,player.exp_level,player.tag,player.clan.name if player.clan is not None else "dehors"))#,ligne_BDD[7]/(ligne_BDD[6]+0.000000000001)*100))
+        rep.add_field(name=player.name,value="<:HdvBot:884202091793506324> Hdv : {} \n<:ExpBot:884202964896608266> Niveau : {} \n<:TagBot:884204003070705754> Tag : {}\n🛡️ Clan:{}".format(player.town_hall,player.exp_level,player.tag,player.clan.name if player.clan is not None else "dehors"))#,ligne_BDD[7]/(ligne_BDD[6]+0.000000000001)*100))
     rep.set_image(url="https://media.discordapp.net/attachments/859386512129654794/884100318936330261/comptes_lie.png")
     rep.set_thumbnail(url=message.mentions[0].avatar_url)
     rep.set_footer(text="Développement av#2616 | Design YohKun#7447 | Empire Galactique",icon_url="https://cdn.discordapp.com/avatars/397116327887896576/93f6ce8dde153200b213ba4ec531dd8f.webp?size=128")
