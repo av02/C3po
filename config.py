e2982850e447743b8aed2e2fedd7dd5c50e15324
av02 <39122034+av02@users.noreply.github.com>
2021-11-05 03:43:39 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,9 @@ config={"Coc":{"mail":os.environ.get("mail"),
               "password":os.environ.get("password")},
         "Discord":{"token":os.environ.get("Token"),
                   "prefix":os.environ.get("prefix")},
-        "bddlink":os.environ.get("DATABASE_URL")
+        "bddlink":os.environ.get("DATABASE_URL"),
+        "liste_clans":["#2PU29PYPR","#29Q29PRY9","#29U9YR0QP","#2LL0UCY89","#2LR9RP20J","#2PYR2V202","#2Y2UVR99P","#2L0JQYUPU","#2LLCPYV9P","#2YU08J8UU"]
+
        }
 print("url db envoyé:",os.environ.get("DATABASE_URL"))
 print("token:",os.environ.get("Token"))
