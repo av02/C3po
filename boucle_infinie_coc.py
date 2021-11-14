1cbcc434d1a5578436067673614f2a0734e8d28e
av02 <39122034+av02@users.noreply.github.com>
2021-11-14 22:35:40 +0100

    Update boucle_infinie_coc.py

--- boucle_infinie_coc.py
+++ boucle_infinie_coc.py
@@ -14,16 +14,19 @@ async def demarage(config,connection_bdd,cocClient):
     #pass
 
 def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
-    clan_tags=["#2PU29PYPR","#29Q29PRY9","#29U9YR0QP","#2LL0UCY89","#2LR9RP20J","#2PYR2V202","#2Y2UVR99P","#2L0JQYUPU","#2LLCPYV9P","#2YU08J8UU"]# mettre ça dans une bdd
-    tagsJoueurs=connection_bdd.get_all_tag()
+    clan_tags = ["#2PU29PYPR","#29Q29PRY9","#29U9YR0QP","#2LL0UCY89","#2LR9RP20J","#2PYR2V202","#2Y2UVR99P","#2L0JQYUPU","#2LLCPYV9P","#2YU08J8UU"]# mettre ça dans une bdd
+    tagsJoueurs = connection_bdd.get_all_tag()
     # connection client coc, non bloquant
     
-    @cocClient.event# quand une attaque de guerre survient
+    
+    
+    
+    
+    # quand une attaque de guerre survient
+    @cocClient.event
     @coc.WarEvents.war_attack(tags=clan_tags)
     async def current_war_stats(attack, war):
-        print("un attaque survint:",attack.attacker.name,"de",attack.attacker.clan.name,"a fait:",attack.stars,"étoiles")
         if attack.attacker.clan.tag in clan_tags and attack.attacker.town_hall>=attack.defender.town_hall:# on controle qu'il est dans un de nos clans
-            print("tag:",attack.attacker_tag,"etoiles:",attack.stars,"th",attack.attacker.town_hall,attack.defender.town_hall,sep="\n\n")
             if attack.attacker.town_hall != attack.defender.town_hall and  attack.attacker.town_hall != attack.defender.town_hall+1: # ce n'est un un x vs x , ni un x vs x-1
                 return   
             connection_bdd.add_score_gdc(attack.attacker_tag,
@@ -36,7 +39,8 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
         else:
             if attack.attacker.town_hall != attack.defender.town_hall:
                 return
-            connection_bdd.add_def_gdc(attack.defender_tag,
+            connection_bdd.add_def_gdc(
+                                        attack.defender_tag,
                                        attack.stars==3
                                         )
 
@@ -46,7 +50,6 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
     @cocClient.event  
     @coc.ClanEvents.member_donations(tags=clan_tags)
     async def on_clan_member_donation(old,new):#TODO controller les odns négatifs
-        print("on a ", old," qui a donné ",new.donations-old.donations,"troupes dans ",old.clan)
         if new.donations<old.donations:# si donné negatifs, alors c'est que debut saison ou quitté le clan
             return
         profil=await cocClient.get_player(old.tag)# on recupere le profil
@@ -56,7 +59,6 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
     @cocClient.event#mise a jour de la bdd quand un membre reçoit
     @coc.ClanEvents.member_received(tags=clan_tags)
     async def on_clan_member_received(old,new):
-        print("on a ", old," qui a reçu ",new.received-old.received,"troupes dans ",old.clan)# pour les logs, aucun interet
         if new.received<old.received:# si le joueur a des reçus negatifs, soit quitté le clan, soit debut de saison
             return
         profil = await cocClient.get_player(old.tag)# on recupere le profil 
@@ -67,12 +69,12 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
     @cocClient.event#mise a jour de la bdd quand un membre change de pseudo
     @coc.PlayerEvents.name(tags= tagsJoueurs)
     async def on_name_change(old,new):
-        print("chnagement de nom",old.name,new.name)
+        print("\033[96mchnagement de nom",old.name,new.name)
         connection_bdd.edit_pseudo(old.tag,new.name)
     @cocClient.event# mise a jour de la bdd quand un membre change d'hdv
     @coc.PlayerEvents.town_hall(tags= tagsJoueurs)
     async def on_th_change(old,new):
-        print("th change", old.th)
+        print("\033[96mth change", old.th)
         connection_bdd.up_hdv(old.tag,new.town_hall)
     
     
@@ -80,6 +82,4 @@ def boucle_infinie_coc(config,connection_bdd,discordClient,cocClient):
     @cocClient.event  
     @coc.PlayerEvents.clan(tags=tagsJoueurs)
     async def on_clan_status_change(old,new):
-        print("il y a {} qui a quitté {} ou rejoint {}".format(old.name,old.clan,new.clan))
         connection_bdd.edit_clan(old.tag,new.clan)
-        pass
