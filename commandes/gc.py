fcdbc32d1382ca14f9d5ee4c4976580c127ee083
av02 <39122034+av02@users.noreply.github.com>
2021-12-27 23:30:47 +0100

    Update gc.py

--- commandes/gc.py
+++ commandes/gc.py
@@ -31,7 +31,7 @@ async def gc(DiscordClient, message,args):#TODO: ajouter le cas de l'autotag, pa
     liste_profilszip =  list(zip(liste_profils,liste_comptes))
     liste_profilszip.sort(reverse = True,key = lambda p:p[0].town_hall*1000+p[0].exp_level)   
     for player in liste_profilszip:   
-        ratio_str = f"\n💯 Ratio Perf : {str(int(player[1][7]//player[1][6]*100))}%"if player[1][6]!=0 else ""
+        ratio_str = f"\n💯 Ratio Perf : {str(int((player[1][7]*100)//player[1][6]))}%"if player[1][6]!=0 else ""
         rep.add_field(name=player[0].name,value="<:HdvBot:884202091793506324> Hdv : {} \n<:ExpBot:884202964896608266> Niveau : {} \n<:TagBot:884204003070705754> Tag : {}\n🛡️ Clan : {}{}".format(player[0].town_hall,player[0].exp_level,player[0].tag,player[0].clan.name if player[0].clan is not None else "Pas de clan",ratio_str))#,ligne_BDD[7]/(ligne_BDD[6]+0.000000000001)*100))
     rep.set_image(url="https://media.discordapp.net/attachments/859386512129654794/884100318936330261/comptes_lie.png")
     rep.set_thumbnail(url=message.mentions[0].avatar_url)
