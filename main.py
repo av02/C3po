750c57d062862f1c0a51c5703999490299c30eac
av02 <39122034+av02@users.noreply.github.com>
2021-12-27 22:56:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,29 +10,31 @@ import signal
 def sigterm():
     print("FIN EXECUTION RECUE")
 def main():
-    print("point 1")
-    signal.signal(signal.SIGTERM,lambda:print("SIGNAL DE FIN EXECUTION RECU"))
+    
+    signal.signal(signal.SIGKILL,lambda:print("SIGNAL DE FIN EXECUTION RECU"))
 
     cocClient= coc.login(email=config["Coc"]["mail"],
                         password=config["Coc"]["password"],
-                        client=coc.EventsClient)
-    print("point 2")
+                        client=coc.EventsClient,
+                        key_count =10,
+                        throttle_limit=100)
+    
     #connection Bdd, non bloquant
     connectionBDD=database_outils.appelsBDD(config["bddlink"],config["liste_clans"])
     connectionBDD.set_cocClient(cocClient)
     #définition du bot
     discordClient=bot_discord.discordClient(connectionBDD,cocClient)
     
-    print("point 3")
+    
     #lancement des evenements coc
     
     boucle_infinie_coc.boucle_infinie_coc(config,connectionBDD,discordClient,cocClient)
     #connectionBDD.appel_bdd("DELETE FROM new WHERE tagig='#2UGJVQGQU'")#SUPPRIMER UN COMPTE, NE PAS METTRE SANS TAG
-    print("point 4")
+    
     discordClient.run(config["Discord"]["token"])#commande blocante pour lancer le bot
 
     
-print("check 0")
+
 
 if __name__=="__main__":
     main()
