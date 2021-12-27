6237db1fbd6e1fac31e0800d857e31b2667d3c3e
av02 <39122034+av02@users.noreply.github.com>
2021-12-27 03:22:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ config={"Coc":{"mail":os.environ.get("mail"),
         "Discord":{"token":os.environ.get("Token"),
                   "prefix":os.environ.get("prefix")},
         "bddlink":os.environ.get("DATABASE_URL"),
-        "liste_clans":["#2PU29PYPR","#29Q29PRY9","#29U9YR0QP","#2LL0UCY89","#2LR9RP20J","#2PYR2V202","#2Y2UVR99P","#2L0JQYUPU","#2LLCPYV9P","#2YU08J8UU"]
+        "liste_clans":["#2PU29PYPR","#29Q29PRY9","#29U9YR0QP","#2LL0UCY89","#2LR9RP20J","#2PYR2V202","#2Y2UVR99P","#2L0JQYUPU","#2LLCPYV9P","#2YU08J8UU","#2LVCU2QQ8"]
 
        }
